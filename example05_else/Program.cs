441f08ba0409be9445a172a4aeffb46b728291b7
sasha <sashau127@icloud.com>
2022-12-14 14:29:50 +0300

    ifelse

---
+++ example05_else/Program.cs
@@ -0,0 +1,13 @@
+﻿ Console.Write("введите имя пользователя:");
+ string username = Console.ReadLine();
+
+ if(username.ToLower() == "маша")
+ {
+Console.WriteLine("ура, это же МАША!!!");
+ }
+else
+ {
+Console.Write("Привет, ");
+Console.WriteLine(username);
+}
+    

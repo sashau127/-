9cd36cd1ec09e17d8f920c422a6caf2e7a1029c3
sasha <sashau127@icloud.com>
2022-12-15 13:30:00 +0300

    обнавлен  реадми

---
+++ example005_ifelse/Program.cs
@@ -0,0 +1,13 @@
+﻿ Console.Write("введите имя пользователя:");
+ string username = Console.ReadLine();
+
+ if(username == "Маша")
+ {
+Console.WriteLine("ура, это же Маша!!!");
+ }
+else
+ {
+Console.Write("Привет,");
+Console.write(username);
+}
+    

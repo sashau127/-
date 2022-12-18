27c265ce6ec410833d75bcd279ed978eab0f0e84
sasha <sashau127@icloud.com>
2022-12-18 15:55:40 +0300

    массив

---
+++ seminar002/example01_array/Program.cs
@@ -0,0 +1,17 @@
+﻿int[] array = {4, 45, 78, 65, 80, 99, 49, 0, 80, 1, 3};
+
+int n = array.Length;
+int find = 80;
+
+int index = 0;
+
+while(index < n)
+{
+
+    if(array[index] == find)
+    {
+        Console.WriteLine(index);
+        break;
+    }
+    index = index+1;
+}
